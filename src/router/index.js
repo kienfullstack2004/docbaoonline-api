abe6faf6b614c408cf7ebd11085d50eeac581ce7
kienfullstack2004 <kiennguyenfullstack2004@gmail.com>
2024-09-23 10:55:15 +0700

    update lan 2

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ const Auth = require("./Auth")
 
 const router = (app) => {
     app.use('/api/v1',Auth);
+    app.use('/',(req,res,next)=>{
+        res.send('<h1>Chào mừng bạn đến với trang website của chúng tôi</h1>')
+    })
 }
 
 module.exports = router;
